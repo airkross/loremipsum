9cfd44559f5bfc479cff49a4e57fe2b1cbe012a0
Ruslan <40143122+airkross@users.noreply.github.com>
2019-05-11 23:03:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nav = document.getElementById('menu');
+var navBtn = document.getElementById('navBtn');
+navBtn.onclick = function(){
+    if(nav.className.match('header-menu')){
+        nav.className = 'active-menu';
+    }
+    else{
+        nav.className = 'header-menu';
+    }
+}
